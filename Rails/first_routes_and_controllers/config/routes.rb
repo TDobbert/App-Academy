68cb8b47025d193f1c8d80b80c548f154d6de2e7
TDobbert <tmdobbert@gmail.com>
2021-07-07 12:30:31 -0700

    finish project

--- Rails/first_routes_and_controllers/config/routes.rb
+++ Rails/first_routes_and_controllers/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :users
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/', to: 'users#index', as: 'users'
+  get 'users/new', to: 'users#new', as:  'new_user'  
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  post 'users/', to: 'users#create', as: 'create_user' 
+  patch 'users/:id', to: 'users#update', as: 'update_user'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy', as: 'delete_user'
 end
