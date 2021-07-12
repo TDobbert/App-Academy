1366332290ac42c6202579394a3a78795a73d2f4
TDobbert <tmdobbert@gmail.com>
2021-07-12 09:57:57 -0700

    added associations for user and artwork models

---
+++ Rails/art_share_API/app/models/artwork.rb
@@ -0,0 +1,10 @@
+class Artwork < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :artist_id, presence: true, uniqueness: true
+  validates image_url, presence: true
+
+  belongs_to :artist,
+  primary_key: :id,
+  foreign_key: :artist_id,
+  class_name: :User
+end
