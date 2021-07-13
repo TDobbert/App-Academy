ef90bef46a67a57a5509ae33fc5e34e406daed38
TDobbert <tmdobbert@gmail.com>
2021-07-13 07:44:33 -0700

    create validations and associations for artwork_shares and add associations to user and artwork

--- Rails/art_share_API/app/models/artwork.rb
+++ Rails/art_share_API/app/models/artwork.rb
@@ -1,10 +1,14 @@
 class Artwork < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-  validates :artist_id, presence: true, uniqueness: true
-  validates image_url, presence: true
+  validates :title, presence: true, uniqueness: { scope: :artist_id }
+  validates :artist_id, presence: true
+  validates :image_url, presence: true, uniqueness: true
 
   belongs_to :artist,
   primary_key: :id,
   foreign_key: :artist_id,
   class_name: :User
+
+  has_many :shared_viewers,
+  through: :artwork_shares,
+  source: :viewer
 end
--- Rails/art_share_API/app/models/user.rb
+++ Rails/art_share_API/app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   foreign_key: :artist_id,
   class_name: :Artwork,
   dependent: :destroy
+
+  has_many :shared_artworks,
+  through: :artwork_shares,
+  source: :artwork
 end
