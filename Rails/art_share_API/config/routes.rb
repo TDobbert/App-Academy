f6cec3f4c369a118713f1948212f1725e950c98e
TDobbert <tmdobbert@gmail.com>
2021-08-19 10:09:50 -0700

    added artworks routes

--- Rails/art_share_API/config/routes.rb
+++ Rails/art_share_API/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # resources :users
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/', to: 'users#index', as: 'users'
-  get 'users/new', to: 'users#new', as:  'new_user'  
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  post 'users/', to: 'users#create', as: 'create_user' 
-  patch 'users/:id', to: 'users#update', as: 'update_user'
-  put 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy', as: 'delete_user'
+  resources :users, only: [:index, :show, :update, :create, :destroy]
+  resources :artworks, only: [:show, :update, :create, :destroy]
 end
