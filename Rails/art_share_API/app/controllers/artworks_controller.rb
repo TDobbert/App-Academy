f75eb58502bb2946f75a158db763d595a9feaa36
TDobbert <tmdobbert@gmail.com>
2021-08-19 10:27:24 -0700

    created RESTful controller methods

--- Rails/art_share_API/app/controllers/artworks_controller.rb
+++ Rails/art_share_API/app/controllers/artworks_controller.rb
@@ -1,7 +1,8 @@
 # app/controllers/artworks_controller.rb
 class ArtworksController < ApplicationController
   def index
-    render json: Artwork.all
+    artist = User.find(params[:user_id])
+    render json: { artworks: artist.artworks }
   end
 
   def create
@@ -18,9 +19,29 @@ class ArtworksController < ApplicationController
     render json: Artwork.find(params[:id])
   end
 
+  def update
+    artwork = Artwork.find(params[:id])
+
+    if artwork.update(artwork_params)
+      render json: artwork
+    else
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    artwork = Artwork.find(params[:id])
+
+    if artwork.destroy
+      render json: artwork
+    else
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def artwork_params
-    params.require(:title).permit(:image_url)
+    params.require(:artwork).permit(:title, :image_url, :artist_id)
   end
 end
