5f7a8b39064266d2d4043159f987168b09afad4a
TDobbert <tmdobbert@gmail.com>
2021-08-23 09:11:13 -0700

    updated routes for artwork_shares

--- Rails/art_share_API/config/routes.rb
+++ Rails/art_share_API/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :update, :create, :destroy]
   resources :artworks, only: [:show, :update, :create, :destroy]
+  resources :artwork_shares, only: [:create, :destroy]
 
   resources :users do
     resources :artworks
