be2d231b2592fa20051c5e383476d46cbcd68143
TDobbert <tmdobbert@gmail.com>
2021-08-23 09:51:42 -0700

    bug fix

--- Rails/art_share_API/app/controllers/artwork_shares_controller.rb
+++ Rails/art_share_API/app/controllers/artwork_shares_controller.rb
@@ -22,6 +22,6 @@ class ArtworkSharesController < ApplicationController
   private
 
   def artwork_share_params
-    params.require(:artwork_shares).permit(:artwork_id, :viewer_id)
+    params.require(:artwork_share).permit(:artwork_id, :viewer_id)
   end
 end
