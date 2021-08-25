e16e43c1c7d91559f189303e25c96433ab6c31e8
TDobbert <tmdobbert@gmail.com>
2021-08-25 14:31:11 -0700

    wrote age method

--- Rails/ninety_nine_cats/app/models/cat.rb
+++ Rails/ninety_nine_cats/app/models/cat.rb
@@ -1,3 +1,17 @@
-class Cat < ApplicationRecord
+require 'action_view'
 
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  validates :birth_date, presence: true
+
+  def age
+    years = Date.today.year - birth_date.year
+    months = Date.today.month - birth_date.month
+    days = Date.today.day - birth_date.day
+
+    from_time = Time.now - years.year - months.month - days.day
+
+    time_ago_in_words(from_time)
+  end
 end
