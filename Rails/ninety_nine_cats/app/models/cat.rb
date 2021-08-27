765858b49ca92ed6d83045013cc5c3143f1ac665
TDobbert <tmdobbert@gmail.com>
2021-08-27 08:58:24 -0700

    db migration and model validations

--- Rails/ninety_nine_cats/app/models/cat.rb
+++ Rails/ninety_nine_cats/app/models/cat.rb
@@ -3,7 +3,11 @@ require 'action_view'
 class Cat < ApplicationRecord
   include ActionView::Helpers::DateHelper
 
-  validates :birth_date, presence: true
+  COLORS = [black, white, orange, yellow, grey, blue].freeze
+
+  validates :birth_date, :name, presence: true
+  validates :color, presence: true, inclusion: { in: COLORS }
+  validates :sex, presence: true, inclusion: { in: [M, F] }
 
   def age
     years = Date.today.year - birth_date.year
