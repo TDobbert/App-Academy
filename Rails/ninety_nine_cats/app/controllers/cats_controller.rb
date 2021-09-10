b95c68a84dcc2810a9d8fb3e76e0958e0ae67555
TDobbert <tmdobbert@gmail.com>
2021-09-10 09:45:46 -0700

    wrote the rest of the cats_controller methods

--- Rails/ninety_nine_cats/app/controllers/cats_controller.rb
+++ Rails/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -14,6 +14,36 @@ class CatsController < ApplicationController
     end
   end
 
+  def new
+    @cat = Cat.new
+    render :cat
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def cat_params
